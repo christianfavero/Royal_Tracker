358f1923cea629f2a13c76744e8bba304a194312
Christian <christianfavero7@gmail.com>
2026-02-22 19:43:38 +0100

    api

--- ChatHandler.php
+++ ChatHandler.php
@@ -1,17 +1,1 @@
-<?php
-require "config.php";
-
-$id_chat = $_POST['id_chat'] ?? 'GLOBAL';
-$id_user_sender = $_POST['id_user_sender'] ?? '';
-$text = $_POST['text'] ?? '';
-
-if(!empty($text) && !empty($id_user_sender)){
-    $stmt = $conn-> prepare("INSERT INTO messages (id_chat, id_user_sender, messaggio VALUES(?, ?, ?)");
-    $stmt->bind_param("sss", $id_chat,$id_user_sender, $text);
-
-    if($stmt->execute()){
-        echo "Successo";
-    }else echo "Errore";
-}
-
-?>
+ 
